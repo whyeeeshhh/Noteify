a953e44951c226e43f0284a2a17af98a7974f84b
Vaishnavi Aju <vaju@my.centennialcollege.ca>
2025-06-05 01:39:23 -0400

    Prepared for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,24 +4,34 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import { ratelimiter } from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin:"http://localhost:5173"
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin:"http://localhost:5173"
+    }
+    ));
 }
-));
+
 app.use(express.json());
 app.use(ratelimiter);
+app.use("/api/notes", notesRoutes)
 
-console.log(process.env.MONGO_URI)
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+if(process.env.NODE_ENV === "production"){
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 connectDB().then(()=>{
     app.listen(process.env.PORT || 5000 ,()=>{
         console.log("Server started on server: 5000")
     });
 })
-
-app.use("/api/notes", notesRoutes)
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
 export const api = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL: BASE_URL,
 })
 
